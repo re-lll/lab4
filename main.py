42c5f48cf7aa5dfb256a4a774c96596cf01612e9
Anya Minko <anya2006minko@gmail.com>
2024-04-19 18:55:22 +0300

    added functions degree and degree2

---
+++ main.py
@@ -0,0 +1,16 @@
+import utils
+
+n = 125
+if utils.degree(n):
+    print(f"{n} is a power of 5")
+else:
+    print(f"{n} is not a power of 5")
+
+import utils
+
+n = 16
+if degree2(n):
+	print(f"{n} is a power of 2")
+else:
+	print(f"{n} is not a power of 2")
+
--- utils.py
+++ utils.py
@@ -11,3 +11,18 @@ def prime(n):
         if n % i == 0:
             return False
     return True
+<<<<<<< Updated upstream
+=======
+
+def degree(n):
+    if n == 1:
+        return True
+    elif n < 1:
+        return False
+    while n % 5 == 0:
+        n /= 5
+    return n == 1
+
+def degree2(n):
+    return n & (n - 1) == 0 and n != 0
+>>>>>>> Stashed changes
